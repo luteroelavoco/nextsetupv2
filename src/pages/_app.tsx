9b772969c26d22a8b505eaf1488922c027df99a8
cientista1 <40244138+cientista1@users.noreply.github.com>
2021-03-08 20:00:37 -0300

    refactor : app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import { AppProps } from 'next/app'
-import GlobalStyle from '../styles/global'
-import { ThemeProvider } from 'styled-components'
-import theme from '../styles/theme'
+import '../styles/global.less'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <ThemeProvider theme={theme}>
+    <>
       <Component {...pageProps} />
-      <GlobalStyle />
-    </ThemeProvider>
+    </>
   )
 }
 
